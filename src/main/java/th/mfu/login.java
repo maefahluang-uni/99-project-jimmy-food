ea12f366947f0c2ecc42eff373420e9aa2660256
Chayapon Nualklub <6531503018@lamduan.mfu.ac.th>
2023-11-13 18:33:57 +0700

    1

--- src/main/java/th/mfu/login.java
+++ src/main/java/th/mfu/login.java
@@ -1,5 +1,15 @@
 package th.mfu;
 
-public class login {
-    
+public class Login {
+
+    private String username;
+    private String password;
+
+    // Constructors, getters, setters, and other methods as needed
+
+    public boolean authenticate() {
+        // Implement authentication logic here
+        // Return true if authentication is successful, false otherwise
+        return false;
+    }
 }
