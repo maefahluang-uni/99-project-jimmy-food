8a7d3a33695da70f85a78168d7fd04bffe23ba0e
Poramet Somboonchai <6531503048@lamduan.mfu.ac.th>
2023-11-26 21:36:33 +0700

    048ีupdate

--- src/main/java/th/mfu/domain/Cart.java
+++ src/main/java/th/mfu/domain/Cart.java
@@ -25,6 +25,9 @@ public class Cart {
     @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
     private List<Item> items = new ArrayList<>();
 
+
+
+
     // Constructors
 
     // Default constructor
--- src/main/java/th/mfu/repository/CartRepository.java
+++ src/main/java/th/mfu/repository/CartRepository.java
@@ -5,7 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import th.mfu.domain.Cart;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
-  
-    Object findAllById(Long id);
 
 }
